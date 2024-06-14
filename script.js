9065d86805aa0cf288f0e730de2176ffcd1fb9dd
hyrasz <136145414+hyrasz@users.noreply.github.com>
2024-06-14 18:21:21 +0700

    update cart

--- script.js
+++ script.js
@@ -56,6 +56,64 @@ window.addEventListener('scroll', function() {
 });*/
 
 // CART
+// Function to format number as currency (Rp. with thousand separators)
+function formatRupiah(number) {
+    return 'Rp. ' + number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+// Function to update total
+function updateTotal() {
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        var price = parseFloat(priceElement.innerText.replace("Rp. ", "").replace(/\./g, ""));
+        var quantity = quantityElement.value;
+        total += price * quantity;
+    }
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName("total-price")[0].innerText = formatRupiah(total);
+}
+
+// Add to Cart
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updateTotal();
+}
+
+function addProductToCart(title, price, productImg) {
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText === title) {
+            alert("You have already added this item to cart!");
+            return;
+        }
+    }
+    var cartBoxContent = `
+      <img src="${productImg}" alt="" class="cart-img">
+      <div class="detail-box">
+        <div class="cart-product-title">${title}</div>
+        <div class="cart-price">${price}</div>
+        <input type="number" value="1" min="1" class="cart-quantity">
+      </div>
+      <i class="fas fa-trash cart-remove"></i>`;
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+    cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener('click', removeCartItem);
+    cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener('change', quantityChanged);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let cartItem = document.querySelector('.cart');
     let navbar = document.querySelector('.navbar'); // Assuming navbar element exists
@@ -111,12 +169,22 @@ document.addEventListener('DOMContentLoaded', () => {
         alert('Your order is placed! Thank you for buying and enjoy your coffee!');
         let orderDetails = [];
 
-        /* Buat masukin pesanannya ke database dan hapus dari cart klo udh berhasil pesan(?)*/
+        // Loop through all cart boxes to get details
+        for (let i = 0; i < cartBoxes.length; i++) {
+            let cartBox = cartBoxes[i];
+            let title = cartBox.getElementsByClassName("cart-product-title")[0].innerText;
+            let price = cartBox.getElementsByClassName("cart-price")[0].innerText;
+            let quantity = cartBox.getElementsByClassName("cart-quantity")[0].value;
+            let priceValue = parseFloat(price.replace('Rp. ', '').replace(/\./g, ""));
+            let subtotalAmount = priceValue * quantity;
+            orderDetails.push({ title: title, price: priceValue, quantity: quantity, subtotal_amount: subtotalAmount });
+        }
+
         // Send data to server using AJAX
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "add_to_database.php", true);
         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function() {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 console.log(xhr.responseText);
             }
@@ -147,63 +215,6 @@ document.addEventListener('DOMContentLoaded', () => {
         updateTotal();
     }
 
-    // Add to Cart
-    function addCartClicked(event) {
-        let button = event.target;
-        let shopProducts = button.parentElement;
-        let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
-        let price = shopProducts.getElementsByClassName("price")[0].innerText;
-        let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
-        addProductToCart(title, price, productImg);
-        updateTotal();
-    }
-
-    function addProductToCart(title, price, productImg) {
-        let cartShopBox = document.createElement("div");
-        cartShopBox.classList.add("cart-box");
-        let cartItems = document.getElementsByClassName("cart-content")[0];
-        let cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
-        for (let i = 0; i < cartItemsNames.length; i++) {
-            if (cartItemsNames[i].innerText === title) {
-                alert("You have already added this item to cart!");
-                return;
-            }
-        }
-        let cartBoxContent = `
-        <img src="${productImg}" alt="" class="cart-img">
-        <div class="detail-box">
-          <div class="cart-product-title">${title}</div>
-          <div class="cart-price">${price}</div>
-          <input type="number" value="1" min="1" class="cart-quantity">
-        </div>
-        <i class="fas fa-trash cart-remove"></i>`;
-        cartShopBox.innerHTML = cartBoxContent;
-        cartItems.append(cartShopBox);
-        cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener('click', removeCartItem);
-        cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener('change', quantityChanged);
-    }
-
-    // Function to format number as currency (Rp. with thousand separators)
-    function formatRupiah(number) {
-        return 'Rp ' + number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    }
-
-    // Function to update total
-    function updateTotal() {
-        var cartContent = document.getElementsByClassName("cart-content")[0];
-        var cartBoxes = cartContent.getElementsByClassName("cart-box");
-        var total = 0;
-        for (var i = 0; i < cartBoxes.length; i++) {
-            var cartBox = cartBoxes[i];
-            var priceElement = cartBox.getElementsByClassName("cart-price")[0];
-            var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-            var price = parseFloat(priceElement.innerText.replace("Rp ", "").replace(".", "").replace(",", "."));
-            var quantity = quantityElement.value;
-            total += price * quantity;
-        }
-        total = Math.round(total * 100) / 100;
-        document.getElementsByClassName("total-price")[0].innerText = formatRupiah(total);
-    }
-
     ready();
 });
+
