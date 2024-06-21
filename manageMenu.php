26af99552a53e5312865ff7b66434bd5f3d0b902
adndrsd1 <adindarizki15@gmail.com>
2024-06-21 23:22:53 +0700

    edit signinController

--- manageMenu.php
+++ manageMenu.php
@@ -136,9 +136,69 @@
                             <td><?php echo $row['namaMenu'] ?></td>
                             <td><?php echo $row['harga'] ?></td>
                             <td class="flex flex-row h-[150px] border-0 justify-center">
-                                <form action="updateMenu.php" method="get">
+                                <form action="" method="get">
                                     <input type="hidden" name="idMenu" value="<?php echo $row['idMenu'] ?>">
-                                    <button class="btn Lightbtn">Update</button>
+                                    <!-- <button class="btn Lightbtn">Update</button> -->
+                                    <button data-modal-target="update-modal" data-modal-toggle="update-modal" class="block text-white bg-[#D1BB9E] hover:bg-blue-800 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2" type="button">
+                                        Update
+                                    </button>
+
+                                    <!-- Main modal for add menu -->
+                                    <div id="update-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                        <div class="relative p-4 w-full max-w-md max-h-full">
+                                            <!-- Modal content -->
+                                            <div class="relative bg-[#D1BB9E] rounded-lg shadow dark:bg-[#D1BB9E]">
+                                                <!-- Modal header -->
+                                                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                                        Update Product
+                                                    </h3>
+                                                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600" data-modal-toggle="update-modal-<?php echo $row['idMenu']; ?>">
+                                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                        </svg>
+                                                        <span class="sr-only">Close modal</span>
+                                                    </button>
+                                                </div>
+                                                <!-- Modal body -->
+                                                <form action="menuController.php" class="p-4 md:p-5" method="post" enctype="multipart/form-data">
+                                                    <input type="hidden" name="idMenu" value="<?php echo $row['idMenu'] ?>">
+                                                    <div class="grid gap-4 mb-2 grid-cols-2">
+                                                        <div class="col-span-2 sm:col-span-1">
+                                                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                                            <select id="category" name="kategori" class="bg-white border border-gray-300 text-[#74512D] text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
+                                                                <option value="Hot Coffee"<?php if($row['kategori'] == "Hot Coffee") echo "selected"; ?>>Hot Coffee</option>
+                                                                <option value="Iced Coffee"<?php if($row['kategori'] == "Iced Coffee") echo "selected"; ?>>Iced Coffee</option>
+                                                                <option value="Pastries"<?php if($row['kategori'] == "Pastries") echo "selected"; ?>>Pastries</option>
+                                                                <option value="Snacks"<?php if($row['kategori'] == "Snacks") echo "selected"; ?>>Snacks</option>
+                                                            </select>
+                                                        </div>
+                                                        <div class="col-span-2">
+                                                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gambar</label>
+                                                            <input type="file" name="gambar" id="gambar" class="bg-white border border-gray-300 text-[#74512D] text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
+                                                            <img src="img/<?php echo $row['gambar']; ?>" alt="Menu" class="mt-2 w-[125px] h-[125px]">
+                                                        </div>
+                                                        <div class="col-span-2">
+                                                            <label for="namaMenu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Menu</label>
+                                                            <input type="text" name="namaMenu" id="namaMenu" value="<?php echo $row['namaMenu']; ?>" class="bg-white border border-gray-300 text-[#74512D] text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="Nama Menu" required="">
+                                                        </div>
+                                                        <div class="col-span-2">
+                                                            <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label>
+                                                            <input type="text" name="harga" id="harga" value="<?php echo $row['harga']; ?>" class="bg-white border border-gray-300 text-[#74512D] text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="Rp 20.000" required="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="flex justify-center items-center">
+                                                        <button type="submit" name="update" class="text-[#74512D] inline-flex items-center bg-[#FFF2E1] hover:bg-[#A79277] focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-3 py-2.5 text-center">
+                                                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                                <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                                                            </svg>
+                                                            Update product
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </form>
                                 <form action="deleteMenu.php" method="get">
                                     <input type="hidden" name="idMenu" value="<?php echo $row['idMenu'] ?>">
